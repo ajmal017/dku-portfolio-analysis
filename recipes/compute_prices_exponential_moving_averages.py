a0b7a13515300150253519550c9a734a0f6da19c
admin <admin>
2020-05-17 16:45:07 +0200

    Recipe PORTFOLIOANALYSIS.compute_prices_exponential_moving_averages saved through notebook notebook editor for compute_prices_exponential_moving_averages

--- recipes/compute_prices_exponential_moving_averages.py
+++ recipes/compute_prices_exponential_moving_averages.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -5,15 +6,20 @@ from dataiku import pandasutils as pdu
 
 # Read recipe inputs
 prices_prepared = dataiku.Dataset("prices_prepared")
-prices_prepared_df = prices_prepared.get_dataframe()
+df = prices_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+final_df = pd.DataFrame()
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+for symbol in df['Symbol'].unique():
+    foo = df[df['Symbol']==symbol]
+    for lag in [10,20,50,12,26]:
+        foo['ema_%s' % str(lag)] = foo['Adj_Close'].ewm(span=lag,min_periods=0,adjust=False,ignore_na=False).mean()
+    foo['macd'] = foo['ema_12']-foo['ema_26']
+    foo['signal_line'] = foo['macd'].ewm(span=9, adjust=False).mean()
+    final_df = final_df.append(foo, ignore_index=True)
 
-
-
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 prices_exponential_moving_averages = dataiku.Dataset("prices_exponential_moving_averages")
-prices_exponential_moving_averages.write_with_schema(prices_exponential_moving_averages_df)
+prices_exponential_moving_averages.write_with_schema(final_df)
