955c1cfc126392dcb2c19136f93f71d5ea78fc9a
admin <admin>
2020-05-21 17:55:29 +0200

    Recipe PORTFOLIOANALYSIS.compute_prices_rsi saved through notebook notebook editor for compute_prices_rsi

--- recipes/compute_prices_rsi.py
+++ recipes/compute_prices_rsi.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -5,16 +6,32 @@ from dataiku import pandasutils as pdu
 
 # Read recipe inputs
 prices_prepared = dataiku.Dataset("prices_prepared")
-prices_prepared_df = prices_prepared.get_dataframe()
+df = prices_prepared.get_dataframe()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['Close_shifted'] = df.groupby(['Symbol'])['Close'].shift(1)
 
-# Compute recipe outputs from inputs
-# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
-# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['Close_shifted'] = df['Close_shifted'].fillna(df['Close'])
+df['perc_change'] = 100*(df['Close']-df['Close_shifted'])/df['Close_shifted']
 
-prices_rsi_df = prices_prepared_df # For this sample code, simply copy input to output
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['up'] = df['perc_change'].apply(lambda x: x if x >0 else 0)
+df['down'] = df['perc_change'].apply(lambda x: x if x<0 else 0)
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['up_mean'] = df.groupby('Symbol')['up'].transform(lambda x: x.rolling(14, 1).mean())
+df['down_mean'] = df.groupby('Symbol')['down'].transform(lambda x: x.rolling(14, 1).mean())
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['rs_sma'] = df['up_mean'] / df['down_mean']
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+df['rsi_sma'] = 100 - 100 / (1+df['rs_sma'])
+df['overbought'] = df['rsi_sma'].apply(lambda x: 1 if x>=70 else 0)
+df['oversold'] = df['rsi_sma'].apply(lambda x: 1 if x <= 30 else 0)
+
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 prices_rsi = dataiku.Dataset("prices_rsi")
 prices_rsi.write_with_schema(prices_rsi_df)
