2dc37c45f399170c1e995d757fb12edd36121654
admin <admin>
2020-05-21 17:56:10 +0200

    Saved recipe: PORTFOLIOANALYSIS.compute_prices_rsi

--- recipes/compute_prices_rsi.py
+++ recipes/compute_prices_rsi.py
@@ -34,4 +34,4 @@ df['oversold'] = df['rsi_sma'].apply(lambda x: 1 if x <= 30 else 0)
 # -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 prices_rsi = dataiku.Dataset("prices_rsi")
-prices_rsi.write_with_schema(prices_rsi_df)
+prices_rsi.write_with_schema(df)
