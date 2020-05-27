b71124334d872223546e0196da02feac5be44913
admin <admin>
2020-05-27 10:50:56 +0200

    Recipe PORTFOLIOANALYSIS.compute_xvuUSlqo saved through notebook notebook editor for compute_xvuUSlqo

--- recipes/compute_xvuUSlqo.py
+++ recipes/compute_xvuUSlqo.py
@@ -1,18 +1,16 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
-
+import os
 
 input_folder = dataiku.Folder('aktVDyjf')
 s3_folder = dataiku.Folder('xvuUSlqo')
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+input_path = input_folder.get_path()
 
-
-with open("local_file_to_upload") as f:
-    folder.upload_stream("name_of_file_in_folder", f)
-
-
-# Write recipe outputs
-images_s3 = dataiku.Folder("xvuUSlqo")
-images_s3_info = images_s3.get_info()
+for file_path in input_folder.list_paths_in_partition():
+    full_path = os.path.join(input_path, file_path.replace('/',''))
+    s3_folder.upload_file(file_path, full_path)
