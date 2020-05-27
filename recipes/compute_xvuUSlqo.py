007b32979b64734aeec622dbdfb533fcbc75de94
admin <admin>
2020-05-27 10:41:43 +0200

    Saved recipe: PORTFOLIOANALYSIS.compute_xvuUSlqo

--- recipes/compute_xvuUSlqo.py
+++ recipes/compute_xvuUSlqo.py
@@ -4,7 +4,13 @@ import pandas as pd, numpy as np
 from dataiku import pandasutils as pdu
 
 
+input_folder = dataiku.Folder('aktVDyjf')
+s3_folder = dataiku.Folder('xvuUSlqo')
 
+
+
+with open("local_file_to_upload") as f:
+    folder.upload_stream("name_of_file_in_folder", f)
 
 
 # Write recipe outputs
